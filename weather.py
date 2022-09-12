8bdc4fc77851e0981472a1cc0f9dfa42ca193c9d
unknown <veselin.trifonov.tt@gmail.com>
2022-09-12 18:30:55 +0200

    Edit imported libraries and temperature round

--- weather.py
+++ weather.py
@@ -1,4 +1,3 @@
-from urllib import request, response
 import requests
 
 API_KEY = "dcb2b92ff42ec75f7493945c932068e4"
@@ -11,7 +10,7 @@ response = requests.get(request_url)
 if response.status_code == 200:
     data = response.json()
     weather = data["weather"][0]["description"]
-    temperature = round(data["main"]["temp"] - 273.15, 2)
+    temperature = round(data["main"]["temp"] - 273.15, 1)
 
     print(f"Weather: {weather}")
     print(f"Temperature: {temperature}°C")
